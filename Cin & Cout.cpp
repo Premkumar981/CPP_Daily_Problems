f37e0663fcf4e4eac3dad96d530c8a1b55bbcd86
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2023-09-03 23:21:53 +0530

    Add files via upload

---
+++ Cin & Cout.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int height;
+	cout<<"Enter your height(in cm): ";
+	cin>>height;
+	cout<<"Your height(in cm) is: "<<height<<endl;
+return 0;	
+}
