af83feb77a5be8b3651c8e6501ff4561b72fe664
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-02-06 21:03:51 +0530

    Add files via upload

---
+++ input_output.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/*int a = 5;
+	float b = 20;
+	double e = 24;
+	char c = 'h';
+	string d = "Prem981";
+	long f = 25000;
+	bool g = a<b;
+	
+	
+	cout<<a<<endl;
+	cout<<b<<endl;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	cout<<e<<endl;
+	cout<<f<<endl;
+	cout<<g<<endl;*/
+	
+	/*string a;
+	cout<<"What is your name: "<<endl;
+	cin>>a;
+	cout<<"Your name is: "<<a<<endl;*/
+	
+	string b;
+	cout<<"enter name "<<endl;
+	getline(cin,b); 
+	cout<<"name is "<<b;
+	
+	
+	
+	return 0;
+}
