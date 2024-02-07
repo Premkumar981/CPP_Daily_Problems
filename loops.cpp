b55bb64feb297b523da8fa897dd76b6708a633ee
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-02-07 09:06:48 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+//	//while loop
+//	int i=0;
+//	while(i<5){
+//		cout<<i;
+//		i++;
+//	}
+
+//	//for loop
+//	
+//	int i;
+//	for(i=0;i<5;i++){
+//		cout<<i;
+//		
+//	}
+
+//	//do while loop
+//	int i=10;
+//	do{
+//		cout<<"DO while "<<i<<endl;
+//		i++;
+//	}
+//	while(i<5);
+//	
+//	int j = 10;
+//	while(j<5){
+//		cout<<"While "<<j<<endl;
+//		j++;
+//	}
+	
+	for(int i=1;i<=10;i++){
+		cout<<"2 x "<<i<<" = "<<(2*i)<<endl;
+	}
+	
+	
+	return 0;
+}
